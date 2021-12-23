3464cc2319c2130eca5ec0c76ec985293bdcfcbf
Youssef <youssef.marzouk@esprit.tn>
2021-12-23 11:08:09 +0100

    implementing socketIO

--- server.js
+++ server.js
@@ -4,6 +4,36 @@ const app = express();
 
 connectDB();
 
+var http = require("http").createServer(app);
+var socketIO = require("socket.io")(http, {
+    cors: {
+        origin: "*"
+    }
+});
+
+var users = [];
+
+
+socketIO.on("connection", function (socket) {
+    
+    socket.on("connected", function (userId) {
+        users[userId] = socket.id;
+        console.log(users);
+    });
+
+    socket.on("sendEvent", function (data) {
+        const message = {
+            "from": data.sender,
+            "message": data.message
+        };
+        socketIO.to(users[data.receiver]).emit("messageReceived", message);
+    });
+ 
+    // socket.on("sendEvent") goes here
+});
+
+
+
 app.use(express.json({ extended: false }));
 
 app.use('/uploads', express.static(__dirname + '/public'));
@@ -12,6 +42,7 @@ app.use('/api/users',require('./Controllers/usersController'))
 app.use('/api/chat',require('./Controllers/chatController'))
 
 
-//const Port = process.env.Port || 3000
-app.listen(process.env.PORT || 3000)
-//app.listen(Port,() => console.log('server started'))
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("server started on port 3000")
+})
